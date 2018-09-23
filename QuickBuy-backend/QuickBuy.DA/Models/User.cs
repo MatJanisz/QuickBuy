5c376c66ba29b4d0000962358034bdc5bb3be078
Mateusz Janiszewski <mateusz.janiszewski@capgemini.com>
2018-09-23 19:26:46 +0200

    initialCommit

---
+++ QuickBuy-backend/QuickBuy.DA/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.DA.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
