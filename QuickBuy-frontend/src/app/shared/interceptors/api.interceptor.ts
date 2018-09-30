1924660fc821b9f0f67feee7bbd016c01e2a6d83
Mateusz Janiszewski <mateusz.janiszewski@capgemini.com>
2018-09-30 13:33:25 +0200

    navigation bar v1.0

---
+++ QuickBuy-frontend/src/app/shared/interceptors/api.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest
+} from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable'; bylo
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const apiReq = req.clone({ url: `http://localhost:49165/api/${req.url}` });
+    return next.handle(apiReq);
+  }
+}
+
---
+++ QuickBuy-frontend/src/app/shared/services/search.service.ts
@@ -0,0 +1,53 @@
+import { ProductModel } from './../models/product.model';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class SearchService {
+  constructor(private _http: HttpClient) {}
+  url = 'product/';
+
+  /*getEvents(searchInput: string): Observable<EventListModel[]> {
+    return this._http.get<EventListModel[]>(
+      this.url + 'geteventswithhostnames/' + searchInput
+    );
+  }
+  getEventParticipantNumber(eventId: string): Observable<number> {
+    return this._http.get<number>(
+      this.url + 'GetNumberEventParticipants/' + eventId
+    );
+  }
+  getPopularEvents(): Observable<EventListModel[]> {
+    return this._http.get<EventListModel[]>(this.url + 'GetMostPopularEvents/');
+  }*/
+
+  getAll(): Observable<ProductModel[]> {
+    return this._http.get<ProductModel[]>(
+      this.url
+    );
+  }
+  getProduct(productId: string): Observable<ProductModel> {
+    return this._http.get<ProductModel>(
+      this.url + 'GetProduct/' + productId
+    );
+  }
+  getProductsByName(name: string): Observable<ProductModel[]> {
+    return this._http.get<ProductModel[]>(
+      this.url + 'GetProductsByName/' + name
+    );
+  }
+  getProductsByCategory(category: string): Observable<ProductModel[]> {
+    return this._http.get<ProductModel[]>(
+      this.url + 'GetProductsByCategory/' + category
+    );
+  }
+  getProductsByNameAndCategory(name: string, category: string): Observable<ProductModel[]> {
+    return this._http.get<ProductModel[]>(
+      this.url + 'GetProductsByNameAndCategory/' + name + '/' + category
+    );
+  }
+
+
+
+}
