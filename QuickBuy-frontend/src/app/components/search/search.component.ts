74f9a7ba1051a7195f854489aa2d6c963285246c
Mateusz Janiszewski <mateusz.janiszewski@capgemini.com>
2018-10-04 10:34:22 +0200

    category dropdown added

--- QuickBuy-frontend/src/app/components/search/search.component.ts
+++ QuickBuy-frontend/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../../shared/enums/category.enum';
 
 @Component({
   selector: 'app-search',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  category = 'abc';
   constructor() { }
 
   ngOnInit() {
+  }
+  selectChangeHandler (event: any) {
+    this.category = event.target.value;
   }
   onSearch(input: string) {
      console.log(input);
